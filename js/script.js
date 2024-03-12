f5ff6f0ca5a9e9c7ae6ca9f69c7c9f10c1af47d8
s4r44d43 <sararez@live.it>
2024-03-12 11:34:03 +0100

    [exercise]

--- js/script.js
+++ js/script.js
@@ -1,13 +1,29 @@
-
+// io sono la destrutturazione di un oggetto
 const { createApp } = Vue;
 
+//io sono un metodo
 createApp({
 
+  //io sono un metodo che restituisce un oggetto
   data(){
 
     return{
-      title : 'Buongiorno vue'
+      title : 'Buongiorno vue',
+      img : {
+        path: './img/legzira.jpg',
+        alt : 'legzira'
+      },
+      isClicked : false
     }
+  },
+
+  // io sono un oggetto anche se mi chiamo metodo
+  methods:{
+
+  },
+
+  // io sono un metodo
+  mounted () {
 
   }
 
